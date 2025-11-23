8e48334167f8fb465128666f2e744c3193709df3
zhangjinjian <1016417093@qq.com>
2025-11-23 14:29:23 +0800

    改名: flutter_tt 改成  flutter_shop

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# flutter_tt
+# flutter_shop
 
 A new Flutter project.
 
---
+++ lib/routes/index.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Widget getRootWidget() {
+  return MaterialApp(
+    // home: const HomePage(),
+    // routes: {'/detail': (_) => const DetailPage()},
+  );
+}
